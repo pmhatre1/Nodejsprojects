e075be18dae6b741df452cc71354efb815c9b17c
pmhatre1 <pmhatre1@asu.edu>
2023-12-24 15:48:23 -0700

    Trial App

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,8 @@
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
 const validator  = require('validator')
 const msg = getNotes()
 
 console.log(msg)
 console.log(validator.isEmail('prath@example.com'))
+console.log(chalk.green("Success"))
